82568853c9b9d9d0b0d8aba0f0f594d50bb79b72
Luis Gustavo <gusouza980@gmail.com>
2021-05-05 16:02:13 -0300

    aaa

--- resources/views/painel/login.blade.php
+++ resources/views/painel/login.blade.php
@@ -5,9 +5,9 @@
 <head>
         
         <meta charset="utf-8" />
-        <title>Gefit - Painel Administrativo</title>
+        <title>Bardega - Painel Administrativo</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Painel de controle da Gefit" name="description" />
+        <meta content="Painel de controle da Bardega" name="description" />
         <meta content="Luis Gustavo de Souza Carvalho" name="author" />
         <!-- App favicon -->
         <link rel="shortcut icon" href="{{asset('admin/images/favicon.ico')}}">
