72c65fd323130111cdc1e3207335fad573418718
Luis Gustavo <gusouza980@gmail.com>
2021-07-15 13:55:54 -0300

    Update

--- resources/views/site/includes/fork.blade.php
+++ resources/views/site/includes/fork.blade.php
@@ -1,3 +1,3 @@
-<a class="thefork" style="position: fixed; bottom: 45%; left: 0; z-index: 9999; cursor: pointer;" data-toggle="modal" data-target="#modalReserva">
+<a class="thefork" style="position: fixed; bottom: 45%; left: 0; z-index: 9999; cursor: pointer; transform: scale(0.7) translateX(-30px);" data-toggle="modal" data-target="#modalReserva">
     <img src="{{asset('site/imagens/botao-reserva-mesa.png')}}" alt="">
 </a>
